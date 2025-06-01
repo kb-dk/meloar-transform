8a7d492b3a106afaa47f6056fde8cf31470b48a3
bolettejurik <baj@kb.dk>
2025-06-01 22:31:41 +0200

    newspapers opdatering af OCR tekstkorpus til og med årgang 1885

--- newspapers/src/main/scripts/add_header1880.sh
+++ newspapers/src/main/scripts/add_header1880.sh
@@ -1,1 +1,1 @@
-( echo "recordID,timestamp,editionId,newspaper_page,fulltext_org" ; cat artikler_1880.csv) > artikler_1880.csv.new
+( echo "recordID,timestamp,editionId,newspaper_page,fulltext_org" ; cat artikler_1885.csv) > artikler_1885.csv.new
